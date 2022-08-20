df9eadce11bf979c388e4b7aad3e351add422353
MrBass <belbizri@projexia.ca>
2022-08-20 16:09:11 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import logo from './logo.svg';
-import {
+ import {
   Navbar, NavbarBrand
 } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
+import React from 'react';
+import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
-function App() {
-  return ( 
-    <div className = "App" >
-   <Navbar dark color="primary">
-    <div className="container">
-      <NavbarBrand href='/'>Ristorante Con fusion</NavbarBrand>
-    </div>
-   </Navbar>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
